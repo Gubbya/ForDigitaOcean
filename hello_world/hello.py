49ef3794cc079ac768194047c7ab7da678f55056
gubby <gubbygubbya@gmail.com>
2025-11-23 00:15:05 +0530

    Ensure the Droplet has Docker & Docker Compose

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -1,8 +1,26 @@
-def main():
-    print("Hello, world!")
-    print("hello how are you!")
-    print("Hello, world! test workflow action")
-    
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+PORT = int(os.getenv("PORT", "8000"))
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(b"Hello, world!\n")
+
+
+def run():
+    server = HTTPServer(("", PORT), Handler)
+    print(f"Serving on port {PORT}")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
 
 if __name__ == "__main__":
-    main()
+    run()
+    
